e60c04b4893ad20b7327fb4b038bbd45aee6d804
alco <you@example.com>
2025-02-21 19:59:58 +0400

    Init

--- terraform-aws-s3-bucket/main.tf
+++ terraform-aws-s3-bucket/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
 # Creating S3 Bucket
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
